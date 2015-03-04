b1bcefbd9763fb32268995708dba41f8a367ce12
Stewman13 <110367@student.sae.edu.au>
2015-03-05 02:08:39 +1000

    TestGrid, *Do Not Like*
    spent some time working on a grid system, that generates a grid then
    allows movement and pathing on said grid. However I have reached a point
    where I believe this is NOT the kind of grid I wish to use, so I am
    going to start again

---
+++ Assets/Scripts/TileClickHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileClickHandler : MonoBehaviour {
+
+	public int tileX;
+	public int tileY;
+	public TileMap map;
+
+	void OnMouseUp() {
+		Debug.Log("click");
+
+		map.MoveSelectedUnitTo(tileX, tileY);
+	}
+}
