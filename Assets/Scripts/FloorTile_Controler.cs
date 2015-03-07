8a64b50374913629c566e22aad0e75d85821a84d
Stewman13 <110367@student.sae.edu.au>
2015-03-08 02:22:52 +1000

    Made Obsolete List/ Made New Grid System
    Moved all assets from previous grid attempt to an Obsolete folder. This
    can be removed later. I only left it there for reference.
    I have also made a new grid based system that is super easy to build
    levels with! it allows you to place any kind of level layout you like,
    all you have to do is change the floor tiles tags

---
+++ Assets/Scripts/FloorTile_Controler.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorTile_Controler : MonoBehaviour {
+
+	//public FloorTile_Controler forward, back, left, right;
+	public float TileGapDistance = 1.0f;
+
+	public bool ForwardAvailable = false;
+	public bool BackAvailable = false;
+	public bool LeftAvailable = false;
+	public bool RightAvailable = false;
+
+
+	void Start(){
+		//set bools to false, so there's no bugs
+		ForwardAvailable = false;
+		BackAvailable = false;
+		LeftAvailable = false;
+		RightAvailable = false;
+	}
+
+	// Run every update
+	void Update () {
+		TileDetector();
+	}
+
+	//Raycasts, to detect tiles in all directions
+	//This allows us to check all tile variables using tags! 
+	void TileDetector(){
+		RaycastHit HitBack;
+		RaycastHit HitForward;
+		RaycastHit HitLeft;
+		RaycastHit HitRight;
+			
+			
+		Ray TileCheckForward = new Ray(transform.position, Vector3.forward);
+		Ray TileCheckBack = new Ray(transform.position, Vector3.back);
+		Ray TileCheckLeft = new Ray(transform.position, Vector3.left);
+		Ray TileCheckRight = new Ray(transform.position, Vector3.right);
+			
+		Debug.DrawRay(transform.position, Vector3.left * TileGapDistance);
+		Debug.DrawRay(transform.position, Vector3.right * TileGapDistance);
+		Debug.DrawRay(transform.position, Vector3.forward * TileGapDistance);
+		Debug.DrawRay(transform.position, Vector3.back * TileGapDistance);
+
+		//Checks if Available
+		if(Physics.Raycast(TileCheckBack, out HitBack, TileGapDistance)){ 
+			if(HitBack.collider.tag == "Available"){
+				BackAvailable = true;
+			}
+		}
+		if(Physics.Raycast(TileCheckForward, out HitForward, TileGapDistance)){ 
+			if(HitForward.collider.tag == "Available"){
+				ForwardAvailable = true;
+			}
+		}
+		if(Physics.Raycast(TileCheckLeft, out HitLeft, TileGapDistance)){ 
+			if(HitLeft.collider.tag == "Available"){
+				LeftAvailable = true;
+			}
+		}
+		if(Physics.Raycast(TileCheckRight, out HitRight, TileGapDistance)){ 
+			if(HitRight.collider.tag == "Available"){
+				RightAvailable = true;
+			}
+		}
+		//Checks if UnAvailable
+		if(Physics.Raycast(TileCheckBack, out HitBack, TileGapDistance)){ 
+			if(HitBack.collider.tag == "UnAvailable"){
+				BackAvailable = false;
+			}
+		}
+		if(Physics.Raycast(TileCheckForward, out HitForward, TileGapDistance)){ 
+			if(HitForward.collider.tag == "UnAvailable"){
+				ForwardAvailable = false;
+			}
+		}
+		if(Physics.Raycast(TileCheckLeft, out HitLeft, TileGapDistance)){ 
+			if(HitLeft.collider.tag == "UnAvailable"){
+				LeftAvailable = false;
+			}
+		}
+		if(Physics.Raycast(TileCheckRight, out HitRight, TileGapDistance)){ 
+			if(HitRight.collider.tag == "UnAvailable"){
+				RightAvailable = false;
+			}
+		}
+	}
+}
