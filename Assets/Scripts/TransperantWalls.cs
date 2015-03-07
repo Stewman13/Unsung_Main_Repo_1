eb52d97ebf527a3c411cda39081960d502787076
Stewman13 <110367@student.sae.edu.au>
2015-03-08 03:17:58 +1000

    Imported See Through Walls
    Took the script from test environment and placed it into the main
    branch! now walls/boxes go see through when passed. Yay.

---
+++ Assets/Scripts/TransperantWalls.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransperantWalls : MonoBehaviour {
+
+	public GameObject player;
+	public LineRenderer line;
+	private BoxCollider col;
+	private Vector3 playerPos;
+	
+	void Start () {
+		line = gameObject.GetComponent<LineRenderer> ();
+		col = gameObject.AddComponent<BoxCollider>();
+		
+	}
+	void Update () {
+		Testing();
+		playerPos = player.transform.position;
+	}
+	
+	void Testing(){
+		
+		//change LineRenderer's Material, to make visible line
+		line.SetWidth (0.5f, 0.5f);
+		line.SetPosition(0, transform.position);
+		line.SetPosition(1, playerPos);
+		line.enabled = true;
+		
+		// make the cube 'attched to the camera's' position, look at player
+		Vector3 direction = player.transform.position - transform.position;
+		transform.right = direction.normalized;
+		
+		// resize collider to fit line renderer
+		col.center = Vector3.right * direction.magnitude /2;
+		col.size = new Vector3(direction.magnitude, 0.5f,0.5f);
+		col.isTrigger = true;
+		
+	}
+
+	//make wall transperant on collision
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.tag == "Box")
+		{		
+			Color newColour = other.renderer.material.color;
+			newColour.a = 0.2f;
+			other.renderer.material.color = newColour;
+		}
+		if(other.gameObject.tag == "Wall")
+		{			
+			Color newColour = other.renderer.material.color;
+			newColour.a = 0.2f;
+			other.renderer.material.color = newColour;
+		}
+	}
+
+	//revert walls apearance on exit
+	void OnTriggerExit(Collider other) 
+	{
+		if(other.gameObject.tag == "Box")
+		{
+			Color newColour = other.renderer.material.color;
+			newColour.a = 1.0f;
+			other.renderer.material.color = newColour;
+		}
+		if(other.gameObject.tag == "Wall")
+		{
+			Color newColour = other.renderer.material.color;
+			newColour.a = 1.0f;
+			other.renderer.material.color = newColour;
+		}
+	}
+}
