fd7c90fb898c5f94347e9b6d22c5e3d1d5b9cab3
Stewman13 <110367@student.sae.edu.au>
2015-03-10 20:08:13 +1000

    Hero Controller
    Hero detects tile under him

---
+++ Assets/Scripts/HeroController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroController : MonoBehaviour {
+
+	public GameObject TileUnderHero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		whereAmI();
+	}
+
+	void whereAmI(){
+		RaycastHit HitDown;
+
+		Ray TileCheckDown = new Ray(transform.position, Vector3.down);
+		Debug.DrawRay(transform.position, Vector3.up * 1.0f);
+
+		if(Physics.Raycast(TileCheckDown, out HitDown, 1.0f)){ 
+			TileUnderHero = HitDown.transform.gameObject;
+			if(TileUnderHero.GetComponent<FloorTile_Controler>().HeroIsOnThisBlock == true){
+				//Do Something... can't think straight
+			}
+		}
+	}
+}
