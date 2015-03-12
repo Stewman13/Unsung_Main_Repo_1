4fd95ec8213127a9b76ede7905e3a7eb0a9ea864
Stewman13 <110367@student.sae.edu.au>
2015-03-13 00:27:35 +1000

    Added Player Detection to Hero.
    CAUTION! THIS IS BROKEN AND UNFINSHED SCRIPT! I will fix it when I get
    time :)

---
+++ Assets/Scripts/Heros_Player_Detection.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Heros_Player_Detection : MonoBehaviour {
+
+	public int SneakSpotDistance;
+	public int RunSpotDistance;
+	public int WalkSpotDistance;
+
+	public RaycastHit HitBack;
+	public RaycastHit HitForward;
+	public RaycastHit HitLeft;
+	public RaycastHit HitRight;
+
+	private Game_Controler _gameCon;
+	private FloorTile_Controler _tileCon;
+
+	// Use this for initialization
+	void Start () {
+		_gameCon = GameObject.Find("Main Camera").GetComponent<Game_Controler>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PlayerDetector();
+	}
+
+	void PlayerDetector(){
+
+		Ray TileCheckForward = new Ray(transform.position, Vector3.forward);
+		Ray TileCheckBack = new Ray(transform.position, Vector3.back);
+		Ray TileCheckLeft = new Ray(transform.position, Vector3.left);
+		Ray TileCheckRight = new Ray(transform.position, Vector3.right);
+
+		//Ray TileCheckForwardWalk = new Ray(transform.position, Vector3.forward);
+		//Ray TileCheckBackWalk = new Ray(transform.position, Vector3.back);
+		//Ray TileCheckLeftWalk = new Ray(transform.position, Vector3.left);
+		//Ray TileCheckRightWalk = new Ray(transform.position, Vector3.right);
+
+		//Ray TileCheckForwardRun = new Ray(transform.position, Vector3.forward);
+		//Ray TileCheckBackRun = new Ray(transform.position, Vector3.back);
+		//Ray TileCheckLeftRun = new Ray(transform.position, Vector3.left);
+		//Ray TileCheckRightRun = new Ray(transform.position, Vector3.right);
+
+		if(_gameCon.playerSneaking == true){
+
+			Debug.DrawRay(transform.position, Vector3.left * SneakSpotDistance);
+			Debug.DrawRay(transform.position, Vector3.right * SneakSpotDistance);
+			Debug.DrawRay(transform.position, Vector3.forward * SneakSpotDistance);
+			Debug.DrawRay(transform.position, Vector3.back * SneakSpotDistance);
+
+
+			if(Physics.Raycast(TileCheckForward, out HitForward, SneakSpotDistance)){ 
+				if(HitBack.collider.tag == "Player"){
+					//DETECTED!
+				}
+			}
+
+			if(Physics.Raycast(TileCheckLeft, out HitLeft, SneakSpotDistance)){ 
+				if(HitBack.collider.tag == "Player"){
+					//DETECTED!
+				}
+			}
+
+			if(Physics.Raycast(TileCheckRight, out HitRight, SneakSpotDistance)){ 
+				if(HitBack.collider.tag == "Player"){
+					//DETECTED!
+				}
+			}
+
+			if(Physics.Raycast(TileCheckBack, out HitBack, SneakSpotDistance)){ 
+				if(HitBack.collider.tag == "Player"){
+					//DETECTED!
+				}
+			}
+		}
+
+		if(_gameCon.playerWalking == true){
+			
+			Debug.DrawRay(transform.position, Vector3.left * WalkSpotDistance);
+			Debug.DrawRay(transform.position, Vector3.right * WalkSpotDistance);
+			Debug.DrawRay(transform.position, Vector3.forward * WalkSpotDistance);
+			Debug.DrawRay(transform.position, Vector3.back * WalkSpotDistance);
+			
+			
+			if(Physics.Raycast(TileCheckForward, out HitForward, WalkSpotDistance)){ 
+				if(HitBack.collider.tag == "Player"){
+					//DETECTED!
+				}
+			}
+			
+			if(Physics.Raycast(TileCheckLeft, out HitLeft, WalkSpotDistance)){ 
+				if(HitBack.collider.tag == "Player"){
+					//DETECTED!
+				}
+			}
+			
+			if(Physics.Raycast(TileCheckRight, out HitRight, WalkSpotDistance)){ 
+				if(HitBack.collider.tag == "Player"){
+					//DETECTED!
+				}
+			}
+			
+			if(Physics.Raycast(TileCheckBack, out HitBack, WalkSpotDistance)){ 
+				if(HitBack.collider.tag == "Player"){
+					//DETECTED!
+				}
+			}
+		}
+
+		if(_gameCon.playerRunning == true){
+			
+			Debug.DrawRay(transform.position, Vector3.left * RunSpotDistance);
+			Debug.DrawRay(transform.position, Vector3.right * RunSpotDistance);
+			Debug.DrawRay(transform.position, Vector3.forward * RunSpotDistance);
+			Debug.DrawRay(transform.position, Vector3.back * RunSpotDistance);
+			
+			
+			if(Physics.Raycast(TileCheckForward, out HitForward, RunSpotDistance)){ 
+				if(HitBack.collider.tag == "Player"){
+					//DETECTED!
+				}
+			}
+			
+			if(Physics.Raycast(TileCheckLeft, out HitLeft, RunSpotDistance)){ 
+				if(HitBack.collider.tag == "Player"){
+					//DETECTED!
+				}
+			}
+			
+			if(Physics.Raycast(TileCheckRight, out HitRight, RunSpotDistance)){ 
+				if(HitBack.collider.tag == "Player"){
+					//DETECTED!
+				}
+			}
+			
+			if(Physics.Raycast(TileCheckBack, out HitBack, RunSpotDistance)){ 
+				if(HitBack.collider.tag == "Player"){
+					//DETECTED!
+				}
+			}
+		}
+	}
+}
