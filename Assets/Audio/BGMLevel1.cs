64079a7b541b55c44f0354d6475cbc4a881eac35
All4thlulz <itachi_uchiha@y7mail.com>
2015-03-16 01:57:13 +1000

    Audio
    Added all of Sam's audio 16-03-2015.

---
+++ Assets/Audio/BGMLevel1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMLevel1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		audio.Play();
+		print ("BGM Play");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDie()
+	{
+		audio.Stop();
+		print ("BGM Stop");
+	}
+}
