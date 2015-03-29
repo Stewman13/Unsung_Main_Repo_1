d4102a7c9792fbe8d71eb8d890ca43a1102b7cba
Luke Jarrett <l.w.jarrett86@gmail.com>
2015-03-29 18:36:02 +1000

    UI Update Pt1
    Just getting things updated with the UI.  Not finished yet.

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+    public bool isQuit = false;
+
+    public void QuitClick()
+    {
+        Application.Quit();
+    }
+
+    public void SceneChanger(int sceneToChangeTo)
+    {
+        Application.LoadLevel(sceneToChangeTo);
+    }
+}
