cd222d0756eac77f3108d3c73cabb1a9485cb3bf
All4thlulz <itachi_uchiha@y7mail.com>
2015-03-30 02:08:12 +1000

    Level Audio
    Level audio setup and implemented the layer system. Found a tile without
    the floor tile controler script and fixed it. Level lighting so that
    things can be seen easier.

--- Assets/Audio/AudioSelector.cs
+++ Assets/Audio/AudioSelector.cs
@@ -3,18 +3,61 @@ using System.Collections;
 
 public class AudioSelector : MonoBehaviour {
 
-	public  AudioClip SoundClipMenu,  SoundClipLevel001,  SoundClipLevel002 ;
-	private AudioSource SoundSourceMenu, SoundSourceLevel001, SoundSourceLevel002;
+	public  AudioClip SoundClipTitleScreen, SoundClipMenu,  SoundClipLevel001, SoundClipLevel002_L1, SoundClipLevel002_L2 ;
+	private AudioSource SoundSourceTitle, SoundSourceMenu, SoundSourceLevel001, SoundSourceLevel002_L1, SoundSourceLevel002_L2;
 
 	public Game_Controler _gameCon;
+
+	public bool Level01Audio = false;
+	public bool Level02Audio = false;
+	public bool Level03Audio = false;
+
+
 
 	void Awake(){
 		_gameCon = GameObject.Find("Main Camera").GetComponent<Game_Controler>();
 	}
 
+	void OnLevelWasLoaded(int level)
+	{
+		print ("level loaded");
+		if (level == 1){
+			print ("Level01 loaded");
+			Level01Audio = true;
+			Level02Audio = false;
+			Level03Audio = false;
+		}
+		if (level == 4){
+			print ("Level02 loaded");
+			Level01Audio = false;
+			Level02Audio = true;
+			Level03Audio = false;
+		}
+		if (level == 5){
+			print ("Level03 loaded");
+			Level01Audio = false;
+			Level02Audio = false;
+			Level03Audio = true;
+		}
+	}
+
 	void Start()
 	{
-		//Level 001 Audio
+		//Level Title Screen
+		if (Application.loadedLevelName == "Title Screen")
+		{
+			//DontDestroyOnLoad(gameObject);
+			SoundSourceTitle = gameObject.AddComponent<AudioSource>();
+			//SoundSource.playOnAwake = false;
+			//SoundSource.rolloffMode = AudioRolloffMode.Logarithmic;
+			SoundSourceTitle.volume = 0.8F;
+			SoundSourceTitle.panLevel = 0;
+			SoundSourceTitle.loop = true;
+			//print ("level 1 audio");
+			TitleScreenAudio();
+		}
+
+		//Level Menu Audio
 		if (Application.loadedLevelName == "Main Menu")
 		{
 			//DontDestroyOnLoad(gameObject);
@@ -25,10 +68,10 @@ public class AudioSelector : MonoBehaviour {
 			SoundSourceMenu.panLevel = 0;
 			SoundSourceMenu.loop = true;
 			//print ("level 1 audio");
-			Level001Audio();
+			MenuAudio();
 		}
 		
-		//Level 002 Audio
+		//Level 001 Audio, only layer 1
 		if (Application.loadedLevelName == "Location_1")
 		{
 			//DontDestroyOnLoad(gameObject);
@@ -39,43 +82,77 @@ public class AudioSelector : MonoBehaviour {
 			SoundSourceLevel001.panLevel = 0;
 			SoundSourceLevel001.loop = true;
 		//	print ("level 2 audio");
-			Level002Audio();
+			Level001Audio();
 			
 		}
 		
-		//Level 003 Audio
+		//Level 002 Audio,  layer 1 and 2
 		if (Application.loadedLevelName == "Location_2")
 		{
 			//DontDestroyOnLoad(gameObject);
-			SoundSourceLevel002 = gameObject.AddComponent<AudioSource>();
+			SoundSourceLevel002_L1 = gameObject.AddComponent<AudioSource>();
+			SoundSourceLevel002_L2 = gameObject.AddComponent<AudioSource>();
 			//SoundSource.playOnAwake = false;
 			//SoundSource.rolloffMode = AudioRolloffMode.Logarithmic;
-			SoundSourceLevel002.volume = 0.7F;
-			SoundSourceLevel002.panLevel = 0;
-			SoundSourceLevel002.loop = true;
+			SoundSourceLevel002_L1.volume = 0.7F;
+			SoundSourceLevel002_L1.panLevel = 0;
+			SoundSourceLevel002_L1.loop = true;
+			SoundSourceLevel002_L2.volume = 0.7F;
+			SoundSourceLevel002_L2.panLevel = 0;
+			SoundSourceLevel002_L2.loop = true;
 			//print ("level 3 audio");
-			Level003Audio();
+			Level002Audio();
 			
 		}
 	}
+
+	void Update(){
+		if (Input.GetKeyUp(KeyCode.Backspace))
+			Application.LoadLevel("Location_2");
+		if (_gameCon.paused == true){
+			if (Level01Audio == true)
+				SoundSourceLevel001.volume = 0.1F;
+			if (Application.loadedLevelName == "Location_2"){
+				SoundSourceLevel002_L1.volume = 0.1F;
+				SoundSourceLevel002_L2.volume = 0.1F;
+			}
+		}
+		if (_gameCon.paused == false){
+			if (Level01Audio == true)
+				SoundSourceLevel001.volume = 0.5F;
+			if (Level02Audio == true){
+				SoundSourceLevel002_L1.volume = 0.4F;
+				SoundSourceLevel002_L2.volume = 0.4F;
+			}
+		}
+
+
+	}
 	
 	//Plays the audio once picked in Awake
-	void Level001Audio()
+	void TitleScreenAudio()
+	{
+		SoundSourceTitle.clip = SoundClipTitleScreen;
+		SoundSourceTitle.Play();
+	}
+	void MenuAudio()
 	{
 		SoundSourceMenu.clip = SoundClipMenu;
 		SoundSourceMenu.Play();
 	}
 	
-	void Level002Audio()
+	void Level001Audio()
 	{
 		SoundSourceLevel001.clip = SoundClipLevel001;
 		SoundSourceLevel001.Play();
 	}
 	
-	void Level003Audio()
+	void Level002Audio()
 	{
-		SoundSourceLevel002.clip = SoundClipLevel002;
-		SoundSourceLevel002.Play();
+		SoundSourceLevel002_L1.clip = SoundClipLevel002_L1;
+		SoundSourceLevel002_L2.clip = SoundClipLevel002_L2;
+		SoundSourceLevel002_L1.Play();
+		SoundSourceLevel002_L2.Play();
 	}
 	
 	
